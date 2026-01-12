1c9d7b974c484c4e444fbcb8b94ba8b51e7554cd
chinacargov7-crypto <chinacargov7@gmail.com>
2026-01-12 23:36:06 +0500

    Chinese website

---
+++ kargo_verileri.js
@@ -0,0 +1,8 @@
+const cargoDatabase = {
+    "EL777": { 
+        status: {tm: "Ýolda", tr: "Yolda", en: "In Transit", ru: "В пути"}, 
+        loc: "Guangzhou", 
+        kg: "12.5 kg", 
+        date: "10.01.2026" 
+    },
+};
